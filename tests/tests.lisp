73c97a382fcddef9e0ee9b41718e19444b4cd503
Gary King <gwking@metabang.com>
2007-02-05 10:05:19 -0500

    Added test for input parameter
    darcs-hash:20070205150519-3cc5d-cc306c505be439cab4bd8450f7e0930799ef269f.gz

--- tests/tests.lisp
+++ tests/tests.lisp
@@ -12,4 +12,11 @@
   test-1
   (ensure-same (parse-integer (shell-command "expr 1 + 1") :junk-allowed t) 2))
 
+(addtest (trivial-shell-test)
+  test-input
+  (ensure-same (parse-integer 
+		(shell-command "wc -c" :input "hello")
+		:junk-allowed t) 
+	       5 :test '=))
+
 
